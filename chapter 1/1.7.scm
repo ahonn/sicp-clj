4b18c6820f886398d81805cc66176ae9f24a9d9d
ahonn <ahonn95@outlook.com>
2016-12-15 10:11:26 +0800

    add chapter 1 1.7

---
+++ chapter 1/1.7.scm
@@ -0,0 +1,17 @@
+(define (good-enough? old-guess next-guess)
+  (< (abs (- new-guess old-guess) old-guess) 0.01)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess (improve guess x))
+      (improve guess x)
+      (sqrt-iter (improve guess x)
+                  x)))
+
+(define (sqrt x)
+  (sqrt-iter 1 x))
