7e07ca63baedf39e74f04e2b8e91cb6ff9676d26
ahonn <ahonn95@outlook.com>
2016-12-18 22:29:28 +0800

    update chapter 1

--- chapter 1/1.8.scm
+++ chapter 1/1.8.scm
@@ -1,3 +1,5 @@
+;;  Newton's method for cube roots is based on the fact that if y is an approximation to the cube root of x, then a better approximation is given by the value (x/y^2+2y)/3
+
 (define (cube x)
   (* x x x))
 
