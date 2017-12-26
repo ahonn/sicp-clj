9e0a727c9b814d88d8b160752eac3c4d74e32b0e
ahonn <ahonn95@outlook.com>
2017-12-26 15:15:08 +0800

    adjust the dir structure

--- src/sicp/chapter01/1_02.clj
+++ src/sicp/chapter01/1_02.clj
@@ -1,5 +1,6 @@
-;; Translate the following expression into prefix form (5 + 4 + (2 - (3 - (6 + 4/5)))) / (3 * (6 - 2) * (2 - 7))
+(ns sicp.chapter01.1-02)
 
+;; Translate the following expression into prefix form (5 + 4 + (2 - (3 - (6 + 4/5)))) / (3 * (6 - 2) * (2 - 7))
 (defn calc []
   (/ (+ 5 4
         (- 2
